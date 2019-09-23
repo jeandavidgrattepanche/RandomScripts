7d251b2165ca92c14fcd58db3e5994c2194e8b96
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2019-09-23 14:39:42 -0400

    Update Test_app_ipad.py
    issue with ' from iPad app

--- Test_app_ipad.py
+++ Test_app_ipad.py
@@ -2,22 +2,22 @@
 
 import re; sys; os
 
-for file in oslistdir(‘DataFolder/’):
-	species=file.split(‘_’)[0]+’_’+file.split(‘_’)[1]
-	os.system(‘species=‘+species)
+for file in oslistdir('DataFolder/'):
+	species=file.split('_')[0]+'_'+file.split('_')[1]
+	os.system('species='+species)
 	filename=file
-	os.system(‘filename=‘+filename)
-	HMMs=HMMs/phag_nonphag-allVall-any3diverse.hmmCAT.hmm
-	os.system(‘HMMs= /phag_nonphag-allVall-any3diverse.hmmCAT.hmm’)	
+	os.system('filename='+filename)
+	HMMs='HMMs/phag_nonphag-allVall-any3diverse.hmmCAT.hmm'
+	os.system('HMMs= /phag_nonphag-allVall-any3diverse.hmmCAT.hmm')	
 
 # run hmmsearch
-	os.system(‘hmmsearch --tblout $species.x.phag_nonphag-allVall-any3diverse.hmmsearchOUT-tbl.txt --cpu 2 $HMMs $filename’)
+	os.system('hmmsearch --tblout $species.x.phag_nonphag-allVall-any3diverse.hmmsearchOUT-tbl.txt --cpu 2 $HMMs $filename')
 
 #then pick the most significant
-	os.system(‘sigfile=${species}_sigHits.txt \
+	os.system('sigfile=${species}_sigHits.txt \
 	sigModel=${sigfile//_sigHits.txt/_sigModels.txt} \
 	echo $sigfile \
 	echo $sigModel \
 	grep -v “^#” $species.x.phag_nonphag-allVall-any3diverse.hmmsearchOUT-tbl.txt | awk ‘$5<=1e-5 && $8<=1e-4 > $sigfile \
-	awk ‘{print $3}’ $sigfile | sort -u > $sigModel’)
-	os.system(‘cp $sigModel TestGenomes/$sigModel’)
+	awk ‘{print $3}’ $sigfile | sort -u > $sigModel')
+	os.system('cp $sigModel TestGenomes/$sigModel')
