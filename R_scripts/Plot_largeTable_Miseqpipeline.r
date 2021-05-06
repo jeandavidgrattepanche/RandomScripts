b38014ce1bdbda075854017d4855e2c76c3b7908
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2021-05-06 18:24:37 -0400

    Update Plot_largeTable_Miseqpipeline.r

--- R_scripts/Plot_largeTable_Miseqpipeline.r
+++ R_scripts/Plot_largeTable_Miseqpipeline.r
@@ -5,27 +5,26 @@ library('phyloseqCompanion')
 library("ggplot2")
 library("plyr")
 library(factoextra)
-env.all <- read.table('NBP1910_envdataCTD_2021d.txt')
-envdata = sample_data(data.frame(cbind(data.frame(scale(env.all[c(5:22)])),env.all[c(1:4)])))
-envdata2 = sample_data(data.frame(scale(env.all[c(5:22)])))
-row.names(envdata2) <- env.all$sample
-env.pca <- prcomp(envdata2, center = TRUE, scale = TRUE)
-summary(env.pca)
-varexp <- env.pca$sdev^2/sum(env.pca$sdev^2)
-env.pca$x %>%
-as.data.frame %>%
-ggplot(aes(x=PC1,y=PC2)) + geom_point(size=4) + theme_bw(base_size=32) + labs(x=paste0("PC1: ", round(varexp[1]*100,1),"%"), y=paste0("PC2: ",round(varexp[2]*100,1), "%")) + theme(legend.position="top")
+#env.all <- read.table('NBP1910_envdataCTD_2021d.txt')
+#envdata = sample_data(data.frame(cbind(data.frame(scale(env.all[c(5:22)])),env.all[c(1:4)])))
+#envdata2 = sample_data(data.frame(scale(env.all[c(5:22)])))
+#row.names(envdata2) <- env.all$sample
+#env.pca <- prcomp(envdata2, center = TRUE, scale = TRUE)
+#summary(env.pca)
+#varexp <- env.pca$sdev^2/sum(env.pca$sdev^2)
+#env.pca$x %>%
+#as.data.frame %>%
+#ggplot(aes(x=PC1,y=PC2)) + geom_point(size=4) + theme_bw(base_size=32) + labs(x=paste0("PC1: ", round(varexp[1]*100,1),"%"), y=paste0("PC2: ",round(varexp[2]*100,1), "%")) + theme(legend.position="top")
 
-library("ggplot2")
-library("plyr")
+
 data.all <- read.table('OTUtable_ingroup_noTtree_part1.tsv')
-mytable = otu_table(data.all, taxa_are_rows=TRUE,errorIfNULL=TRUE)
+mytable = otu_table(cbind(data.all[c(0)],data.all[c(16:111)]), taxa_are_rows=TRUE,errorIfNULL=TRUE)
 env.all <- read.table('NBP1910_envdataCTD_2021.txt')
 envdata = sample_data(env.all)
-tree <- phytools::read.newick('rename2.tree')
-phy_tree(tree, errorIfNULL=TRUE)
-test = read.table("VTaxo.txt", header=TRUE)
-testb <- as.matrix(test[,-1])
+#tree <- phytools::read.newick('rename2.tree')
+#phy_tree(tree, errorIfNULL=TRUE)
+#test = read.table("VTaxo.txt", header=TRUE)
+testb <- as.matrix(data.all[c(0:15)])
 TAX <- tax_table(testb)
 physeq <- phyloseq(mytable, envdata, tree, TAX)
 pico <- subset_samples(physeq, size == "pico")
