9e666de2bbbd169988d1bb3a29f712446b9acce5
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2021-05-07 17:31:09 -0400

    Update Plot_largeTable_Miseqpipeline.r

--- R_scripts/Plot_largeTable_Miseqpipeline.r
+++ R_scripts/Plot_largeTable_Miseqpipeline.r
@@ -1,35 +1,23 @@
-setwd('/Users/jaydiii/Desktop/JD_desktop/R_MiSeq/R_all')
+setwd('/home/tuk61790/HPCr/')
 library('ape')
 library('phytools')
 library('phyloseqCompanion')
 library("ggplot2")
 library("plyr")
 library(factoextra)
-#env.all <- read.table('NBP1910_envdataCTD_2021d.txt')
-#envdata = sample_data(data.frame(cbind(data.frame(scale(env.all[c(5:22)])),env.all[c(1:4)])))
-#envdata2 = sample_data(data.frame(scale(env.all[c(5:22)])))
-#row.names(envdata2) <- env.all$sample
-#env.pca <- prcomp(envdata2, center = TRUE, scale = TRUE)
-#summary(env.pca)
-#varexp <- env.pca$sdev^2/sum(env.pca$sdev^2)
-#env.pca$x %>%
-#as.data.frame %>%
-#ggplot(aes(x=PC1,y=PC2)) + geom_point(size=4) + theme_bw(base_size=32) + labs(x=paste0("PC1: ", round(varexp[1]*100,1),"%"), y=paste0("PC2: ",round(varexp[2]*100,1), "%")) + theme(legend.position="top")
+source('/home/tuk61790/HPCr/multiplot.R', chdir = TRUE)
 
-
-data.all <- read.table('OTUtable_ingroup_noTtree_part1.tsv')
+data.all <- read.table('OTUtable_ingroup_noTtree_part1.txt')
 mytable = otu_table(cbind(data.all[c(0)],data.all[c(16:111)]), taxa_are_rows=TRUE,errorIfNULL=TRUE)
 env.all <- read.table('NBP1910_envdataCTD_2021.txt')
 envdata = sample_data(env.all)
-#tree <- phytools::read.newick('rename2.tree')
-#phy_tree(tree, errorIfNULL=TRUE)
-#test = read.table("VTaxo.txt", header=TRUE)
 testb <- as.matrix(data.all[c(0:15)])
 TAX <- tax_table(testb)
-physeq <- phyloseq(mytable, envdata, tree, TAX)
-pico <- subset_samples(physeq, size == "pico")
-nano <- subset_samples(physeq, size == "nano")
-micro <- subset_samples(physeq, size == "micro")
+physeq <- phyloseq(mytable, envdata, TAX)
+#pico <- subset_samples(physeq, size == "pico")
+#nano <- subset_samples(physeq, size == "nano")
+#micro <- subset_samples(physeq, size == "micro")
+
 #PCoA <- ordinate(physeq, "PCoA", distance = "canberra")
 dUNIFRAC = UniFrac(physeq, weighted=TRUE, normalized=FALSE, fast=TRUE)
 PCoA <- ordinate(physeq, "PCoA", distance = dUNIFRAC)
@@ -38,31 +26,37 @@ ordplotPplus <- ordplotP + geom_point(size=3) + stat_ellipse(geom = "polygon", t
 quartz()
 plot(ordplotPplus)
 
-#PCoAm <- ordinate(micro, "PCoA", distance = "canberra")
-dUNIFRACm = UniFrac(micro, weighted=TRUE, normalized=FALSE, fast=TRUE)
-PCoAm <- ordinate(micro, "PCoA", distance = dUNIFRACm)
-ordplotPm <- plot_ordination(micro, PCoAm, color="Lat", shape="depth",label="sample",title="Micropk")
-ordplotPmplus <- ordplotPm + geom_point(size=3) + stat_ellipse(geom = "polygon", type="norm", alpha=0.4, aes(fill=depth))
-quartz()
-plot(ordplotPmplus)
+sizes <- c("micro","nano","pico")
+indices <- c("bray","jaccard","chao") #"raup","chisq"
+list <- list()
+i = 0
+for(ind in indices){
+	for(sizedata in sizes){
+		i = i + 1
+		named <- paste(ind,sizedata,sep = "-")
+		print(named)
+		PCoAm <- ordinate(subset_samples(physeq, size == sizedata), "PCoA", distance = ind)
+		ordplotPm <- plot_ordination(subset_samples(physeq, size == sizedata), PCoAm, color="Lat", shape="depth",label="sample",title=named)
+		ordplotPmplus <- ordplotPm + geom_point(size=3)
+		assign(paste("p",i,sep=""), ordplotPmplus)
+		list[[length(list)+1]] <- paste("p",i,sep="")
+#		plot(get(paste("p",i,sep="")))
+	}
+}
+pdf('PCoAs.pdf')
+multiplot(p1,p2,p3,p4,p5,p6,p7,p8,p9,cols=3)
+dev.off()
 
-#PCoAn <- ordinate(nano, "PCoA", distance = "canberra")
-dUNIFRACn = UniFrac(nano, weighted=TRUE, normalized=FALSE, fast=TRUE)
-PCoAn <- ordinate(nano, "PCoA", distance = dUNIFRACn)
-ordplotPn <- plot_ordination(nano, PCoAn, color="Lat", shape="depth",label="sample",title="Nanopk")
-quartz()
-plot(ordplotPn)
-
-#PCoAn <- ordinate(nano, "PCoA", distance = "canberra")
-dUNIFRACp = UniFrac(pico, weighted=TRUE, normalized=FALSE, fast=TRUE)
-PCoAp <- ordinate(pico, "PCoA", distance = dUNIFRACp)
-ordplotPp <- plot_ordination(pico, PCoAp, color="Lat", shape="depth",label="sample",title="Picopk")
-quartz()
-plot(ordplotPp)
-
-ordMDS <- metaMDS(t(otu_table(physeq)))
-ordMDS.fit <- envfit(ordMDS ~ Lat + Long + bprod + pprod_Sun + pprod_PAR + ice + airT + depth_m + PAR1 + PAR2 + salinity1 + timeJ + latitude + longitude + oxygen1 + oxygenSaturation + fluorescence + beamTrans + waterT1 + conductivity1 + depSM + size_num, data=sample.data.table(physeq), perm=999, na.rm = TRUE)
-plot(ordMDS, dis="site")
-plot(ordMDS.fit)
-text(ordMDS, display="sites")
-ordMDS.fit
+list <- list()
+j = 0
+for(sizedata in sizes){
+	j = j + 1
+	print(sizedata)
+	ordMDS <- metaMDS(t(otu_table(subset_samples(physeq, size == sizedata))))
+	ordMDS.fit <- envfit(ordMDS ~ Lat + Long + bprod + pprod_Sun + pprod_PAR + ice + airT + depth_m + PAR1 + PAR2 + salinity1 + timeJ + latitude + longitude + oxygen1 + oxygenSaturation + fluorescence + beamTrans + waterT1 + conductivity1 + depSM + size_num, data=sample.data.table(sizedata), perm=999, na.rm = TRUE)
+	part123 <- c(plot(ordMDS, dis="site") , plot(ordMDS.fit) , text(ordMDS, display="sites"))
+	assign(paste("q",j,sep=""), part123)
+}
+pdf('NMDS.pdf')
+multiplot(q1,q2,q3,q4,q5,q6,q7,q8,q9,cols=3)
+dev.off()
