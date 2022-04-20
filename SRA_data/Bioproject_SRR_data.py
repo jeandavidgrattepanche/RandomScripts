eef7e9c7e64d12e9777b7be3589bda29ee267b6a
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2022-04-20 16:56:21 -0400

    Update Bioproject_SRR_data.py

--- SRA_data/Bioproject_SRR_data.py
+++ SRA_data/Bioproject_SRR_data.py
@@ -19,20 +19,14 @@ def main():
 			time.sleep(2)
 
 		for SRRnumber in open("SRR_numbers.txt"):	
-			lr=0; check= 0
-		# 		print(SRRnumber[0:3])
 			if SRRnumber[0:3] != "Run" and SRRnumber.split('\n')[0] != "":
 				print("Downloading and preparing ",SRRnumber.split('\n')[0])
-				while check == 0:
-					os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
+				os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
 	elif BioP[0].lower() == 'n':
 		for SRRnumber in open(bioproject_list):	
-			lr=0; check= 0
-		# 		print(SRRnumber[0:3])
 			if SRRnumber[0:3] != "Run" and SRRnumber.split('\n')[0] != "":
 				print("Downloading and preparing ",SRRnumber.split('\n')[0])
-				while check == 0:
-					os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
+				os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
 	else:
 		print("************** \n Error in arguments \n example python3 Bioproject_SRR_ data.py list_of_Accession [y/n] \n y if you use Bioproject accession \n n if you use SRR numbers \n ********************")					
 main()
