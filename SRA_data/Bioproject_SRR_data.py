a4d16e1a532af6a58f438eaf3c826e9afcfdc662
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2022-04-20 16:35:08 -0400

    Update Bioproject_SRR_data.py

--- SRA_data/Bioproject_SRR_data.py
+++ SRA_data/Bioproject_SRR_data.py
@@ -14,8 +14,8 @@ def main():
 			handle = Entrez.esearch(db="sra", term=dataname, RetMax=1000)
 			records = Entrez.read(handle)
 			print(dataname, '\t',records["Count"])
-# 			os.system("esearch -db sra -query "+dataname+' | efetch -format runinfo | cut -d "," -f 1 >> SRR_numbers.txt')
-			os.system("esearch -db sra -query "+dataname+" | efetch -format runinfo > "+dataname+'2.tsv | cut -d "," -f 1 > SRR_numbers.txt')
+			os.system("esearch -db sra -query "+dataname+' | efetch -format runinfo | cut -d "," -f 1 >> SRR_numbers.txt')
+# 			os.system("esearch -db sra -query "+dataname+" | efetch -format runinfo > "+dataname+'2.tsv | cut -d "," -f 1 > SRR_numbers.txt')
 			time.sleep(2)
 
 		for SRRnumber in open("SRR_numbers.txt"):	
@@ -24,7 +24,7 @@ def main():
 			if SRRnumber[0:3] != "Run" and SRRnumber.split('\n')[0] != "":
 				print("Downloading and preparing ",SRRnumber.split('\n')[0])
 				while check == 0:
-					os.system('fasterq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + '--outdir $PWD/ExampleFiles/RawData/')
+					os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
 	elif BioP[0].lower() == 'n':
 		for SRRnumber in open(bioproject_list):	
 			lr=0; check= 0
@@ -32,7 +32,7 @@ def main():
 			if SRRnumber[0:3] != "Run" and SRRnumber.split('\n')[0] != "":
 				print("Downloading and preparing ",SRRnumber.split('\n')[0])
 				while check == 0:
-					os.system('fasterq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + '--outdir $PWD/ExampleFiles/RawData/')
+					os.system('fastq-dump --split-files --gzip '+SRRnumber.split('\n')[0] + ' --outdir $PWD/ExampleFiles/RawData/')
 	else:
 		print("************** \n Error in arguments \n example python3 Bioproject_SRR_ data.py list_of_Accession [y/n] \n y if you use Bioproject accession \n n if you use SRR numbers \n ********************")					
 main()
